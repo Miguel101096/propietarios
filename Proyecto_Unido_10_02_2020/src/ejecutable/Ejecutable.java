64190268c38088347165f214f64921ca08c288b1
Patrick <52832821+PatrickC96@users.noreply.github.com>
2020-02-13 13:18:42 -0500

    Add files via upload

--- Proyecto_Unido_10_02_2020/src/ejecutable/Ejecutable.java
+++ Proyecto_Unido_10_02_2020/src/ejecutable/Ejecutable.java
@@ -2,6 +2,8 @@ package ejecutable;
 
 
 import modelo.ARCHIVOS_PLANOS;
+import modelo.Genera_Cuenta;
+import modelo.PASAJERO;
 import vistas.PRINCIPAL;
 
 
@@ -10,10 +12,16 @@ public class Ejecutable {
     public static void main(String[] args) {
 
 //      ARCHIVOS_PLANOS obj = new ARCHIVOS_PLANOS();       
-//      obj.leerArchivo("172526388-1");
-//        PRINCIPAL p = new PRINCIPAL();
-//        p.setLocationRelativeTo(null);
-//        p.setVisible(true);
+////      obj.leerArchivo("172526388-1");
+        PRINCIPAL p = new PRINCIPAL();
+        p.setLocationRelativeTo(null);
+        p.setVisible(true);
+        
+        
+//        
+//        Genera_Cuenta gc = new Genera_Cuenta();
+//        PASAJERO pasajero = gc.generaDatosPasajero("175168097-4");
+//        System.out.println(pasajero.getNombre());
       
     }
     
